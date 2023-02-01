3a479b96fe4efa0c4640d16620f24fddb7886c84
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2023-02-01 21:41:09 +0300

    feat: add user register function

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,13 +1,41 @@
 import User from '../models/user.js';
 import Lesson from '../models/lesson.js';
 import game from '../models/game.js';
+import bcrypt from 'bcryptjs';
+import { validationResult } from 'express-validator'; 
 
 class Controller {
   async register(req, res) {
     try {
-      res.json('register');
+      // validation user input
+      const validationError = validationResult(req);
+
+      if(!validationError.isEmpty()) {
+        return res.status(400).json({message: `input validation error: ${validationError.errors[0].msg}`});
+      }
+
+      const username = req.body.username;
+      const password = req.body.password;
+
+      // check for user exist
+      const findUserName = await User.findOne({username});
+
+      if (findUserName !== null) {
+        return res.status(400).json({message: 'user exists'});
+      }
+
+      // crypt password
+      const passwordHash = bcrypt.hashSync(password, 5);
+
+      // save user
+      const user = new User({username, password: passwordHash});
+      await user.save();
+
+      return res.json({message: 'user registered'});
+
     } catch (error) {
       console.log(error);
+      res.status(400).json({message: 'registration error'});
     }
   }
 
--- server/src/route.js
+++ server/src/route.js
@@ -1,9 +1,13 @@
 import Router from 'express';
 import Controller from './controller.js';
+import { check } from 'express-validator'; 
 
 const router = new Router();
 
-router.post('/register', Controller.register);
+router.post('/register', [
+  check('username', 'invalid username length').isLength({min: 3, max: 12}),
+  check('password', 'invalid password length').isLength({min: 3, max: 12}),
+], Controller.register);
 router.post('/auth', Controller.auth);
 
 export default router;
