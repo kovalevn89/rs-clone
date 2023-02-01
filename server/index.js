5ea8ab8f8aeb0b2d8ba5dbe04f5a091094844085
Kovalev Nikolay <kovalev.nikolay.89@gmail.com>
2023-02-01 16:08:08 +0300

    feat: add mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 5000;
 const app = express();
 
-function run() {
+app.use(express.json());
+
+async function run() {
   try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect('mongodb+srv://typego:pa123sword@cluster0.il8qg3f.mongodb.net/?retryWrites=true&w=majority');
     app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
   } catch (error) {
     console.log(error);
