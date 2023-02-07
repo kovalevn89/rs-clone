5c7af3bfc375ee216efb38890cbda4dc73f09609
AnnaFeona <glavatskikh.anna@gmail.com>
2023-02-07 11:52:31 +0300

    refactor: replace input handler function to a separate file

--- typego/src/components/view/training/keybordHandlers.ts
+++ typego/src/components/view/training/keybordHandlers.ts
@@ -1,3 +1,42 @@
-export const keyDowmHandler = (e: KeyboardEvent): void => {
-  
-}
+import { Status } from '../../types';
+import Keyboard from '../keyboard/keyboard';
+import Text from './text';
+
+export const keyDowmHandler = (e: KeyboardEvent, keyboard: Keyboard, text: Text): void => {
+  e.preventDefault();
+  const id = e.code.toLowerCase();
+
+  keyboard.activate(id, Status.active);
+  let { index } = text;
+  const { words } = text;
+
+  if (e.code === 'ShiftRight' || e.code === 'CapsLock' || e.code === 'ShiftLeft') {
+    text.updateIndex(index);
+  } else if (e.code === 'Backspace') {
+    text.updateLetterStatus(index, Status.reset);
+    if (index > 0) {
+      index -= 1;
+      words[index].dataset.fix = 'true';
+    }
+    text.updateLetterStatus(index, Status.reset);
+  } else if (words[index].textContent === e.key) {
+    if (words[index].dataset.fix && !words[index].dataset.correct) {
+      text.updateLetterStatus(index, Status.fixed);
+    } else {
+      text.updateLetterStatus(index, Status.correct);
+      words[index].dataset.correct = 'true';
+    }
+    index += 1;
+  } else {
+    text.updateLetterStatus(index, Status.incorrect);
+    words[index].dataset.correct = '';
+    index += 1;
+  }
+
+  text.updateIndex(index);
+  text.updateActive();
+};
+
+// export const keyUpHandler = (e: )
+
+export default keyDowmHandler;
--- typego/src/components/view/training/textInput.ts
+++ typego/src/components/view/training/textInput.ts
@@ -1,7 +1,6 @@
-import { Status } from '../../types';
 import Keyboard from '../keyboard/keyboard';
+import { keyDowmHandler } from './keybordHandlers';
 import Text from './text';
-// import keyPressSound from '../../../assets/sounds/9.mp3';
 
 class TextInput {
   input;
@@ -21,41 +20,9 @@ class TextInput {
   listen(keyboard: Keyboard, text: Text): void {
     const press = new Audio();
     // press.src = keyPressSound;
-    const { words } = text;
-    let { index } = text;
     this.input.addEventListener('keydown', (e) => {
-      e.preventDefault();
-      keyboard.init();
-      const id = e.code.toLowerCase();
-      console.log(e.code);
-      keyboard.activate(id, Status.active);
+      keyDowmHandler(e, keyboard, text);
       press.play();
-
-      if (e.code === 'ShiftRight' || e.code === 'CapsLock' || e.code === 'ShiftLeft') {
-        text.updateIndex(index);
-      } else if (e.code === 'Backspace') {
-        text.updateLetterStatus(index, Status.reset);
-        if (index > 0) {
-          index -= 1;
-          words[index].dataset.fix = 'true';
-        }
-        text.updateLetterStatus(index, Status.reset);
-      } else if (words[index].textContent === e.key) {
-        if (words[index].dataset.fix && !words[index].dataset.correct) {
-          text.updateLetterStatus(index, Status.fixed);
-        } else {
-          text.updateLetterStatus(index, Status.correct);
-          words[index].dataset.correct = 'true';
-        }
-        index += 1;
-      } else {
-        text.updateLetterStatus(index, Status.incorrect);
-        words[index].dataset.correct = '';
-        index += 1;
-      }
-
-      text.updateIndex(index);
-      text.updateActive();
     });
 
     this.input.addEventListener('keyup', () => {
