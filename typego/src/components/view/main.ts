a81dfd1219117c9dc408de0b26302867f24d63c9
Kovalev Nikolay <kovalev.nikolay.89@gmail.com>
2023-02-10 13:42:45 +0300

    feat: add proposales block code and style

--- typego/src/components/view/main.ts
+++ typego/src/components/view/main.ts
@@ -1,8 +1,8 @@
 import { createElement, removeChild } from '../helper';
 import image1 from '../../assets/png/image1.png';
 import proposale1 from '../../assets/png/propo1.png';
-// import proposale2 from '../../assets/png/propo2.png';
-// import proposale3 from '../../assets/png/propo3.png';
+import proposale2 from '../../assets/png/propo2.png';
+import proposale3 from '../../assets/png/propo3.png';
 
 class Main {
   // constructor() {
@@ -37,6 +37,7 @@ class Main {
       createElement('div', 'ellipse2', proposalesBlock);
       const wrapper2 = createElement('div', 'block_wrapper', proposalesBlock);
 
+      // propo1
       const card1 = createElement('div', 'proposales__card', wrapper2);
       const cardLeft1 = createElement('div', 'card__left', card1);
       const cardImage1 = createElement('div', 'card__image', cardLeft1);
@@ -45,11 +46,46 @@ class Main {
       const cardContext1 = createElement('div', 'card__context', cardRight1);
       createElement('h2', 'card__context-caption', cardContext1).textContent = 'Научись печатать вслепую';
       createElement('div', 'card__context-text', cardContext1).textContent = 'Ускорь прогресс обучения слепой печати и развивай ценные навыки набора текста.';
-      const contextLink = createElement('div', 'card__context-link', cardContext1);
-      createElement('div', 'context-link__button', contextLink).textContent = 'Перейти к обучению';
+      const contextLink1 = createElement('div', 'card__context-link', cardContext1);
+      createElement('div', 'context-link__button', contextLink1).textContent = 'Перейти к обучению';
 
-      createElement('div', 'proposales__card', wrapper2);
-      createElement('div', 'proposales__card', wrapper2);
+      // propo2
+      const card2 = createElement('div', 'proposales__card', wrapper2);
+      card2.style.marginTop = '2.5vw';
+      card2.style.marginLeft = '2.7vw';
+      const cardLeft2 = createElement('div', 'card__left', card2);
+      cardLeft2.style.width = '25%';
+      const cardImage2 = createElement('div', 'card__image', cardLeft2);
+      cardImage2.style.background = `url(${proposale2}) center no-repeat`;
+      const cardRight2 = createElement('div', 'card__right', card2);
+      cardRight2.style.order = '-1';
+      const cardContext2 = createElement('div', 'card__context', cardRight2);
+      createElement('h2', 'card__context-caption', cardContext2).textContent = 'Пройди тест скорости печати';
+      createElement('div', 'card__context-text', cardContext2).textContent = 'Узнай свою скорость печати и удиви приятелей или руководство полученным сертификатом.';
+      const contextLink2 = createElement('div', 'card__context-link', cardContext2);
+      createElement('div', 'context-link__button', contextLink2).textContent = 'Перейти к тесту скоростной печати';
+
+      // propo3
+      const card3 = createElement('div', 'proposales__card', wrapper2);
+      card3.style.marginTop = '2.5vw';
+      const cardLeft3 = createElement('div', 'card__left', card3);
+      const cardImage3 = createElement('div', 'card__image', cardLeft3);
+      cardImage3.style.background = `url(${proposale3}) center no-repeat`;
+      const cardRight3 = createElement('div', 'card__right', card3);
+      const cardContext3 = createElement('div', 'card__context', cardRight3);
+      createElement('h2', 'card__context-caption', cardContext3).textContent = 'Учись играя';
+      createElement('div', 'card__context-text', cardContext3).textContent = 'Каждая игра направлена на развитие техники вашей печати. Играйте, следите за прогрессом, становитесь ближе к своей цели!';
+      const contextLink3 = createElement('div', 'card__context-link', cardContext3);
+      createElement('div', 'context-link__button', contextLink3).textContent = 'Перейти к играм';
+
+      // registration
+      const registration = createElement('div', 'registration', wrapper2);
+      const registrationCaption = createElement('h2', 'registration-caption', registration);
+      registrationCaption.textContent = 'Печатать вслепую - легко';
+      const registrationText = createElement('div', 'registration-text', registration);
+      registrationText.textContent = 'Регистрируйся прямо сейчас, проходи обучение и следи за своим прогрессом';
+      const registrationButton = createElement('div', 'registration-button', registration);
+      registrationButton.textContent = 'Создать аккаунт';
     }
   }
 
