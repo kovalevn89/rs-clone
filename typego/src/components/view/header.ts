94eb6d96d4823b5ad5c90d0dfe641140bd7947cc
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2023-02-12 02:50:19 +0300

    feat: add burger menu

--- typego/src/components/view/header.ts
+++ typego/src/components/view/header.ts
@@ -15,13 +15,44 @@ class Header {
         if (header !== null) {
           const wrapper = createElement('div', 'header__wrapper', header);
 
+          // const burger =
+          // createElement('div', 'header__burger', wrapper);
+          // console.log(burger);
+          // createElement('div', 'logo__caption', logo).textContent = 'TypeGo';
           // logo
           const logo = createElement('div', 'header__logo', wrapper);
+          const burger = createElement('div', 'header__burger', logo);
+          createElement('div', 'burger__line', burger);
+          createElement('div', 'burger__line', burger);
+          createElement('div', 'burger__line', burger);
           createElement('div', 'logo__caption', logo).textContent = 'TypeGo';
+
+          burger.addEventListener('click', () => {
+            const nav = document.querySelector('.nav');
+
+            if (nav !== null) {
+              nav.classList.toggle('hidden');
+            }
+          });
+
+          window.addEventListener('click', (event) => {
+            const element: HTMLElement = event.target as HTMLElement;
+            if (!element.closest('.nav')) {
+              if (!element.closest('.header__burger')) {
+                const nav = document.querySelector('.nav');
+
+                if (nav !== null) {
+                  if (!nav.classList.contains('hidden')) {
+                    nav.classList.add('hidden');
+                  }
+                }
+              }
+            }
+          });
 
           // menu
           const menu = createElement('nav', 'header__menu', wrapper);
-          const nav = createElement('nav', 'nav', menu);
+          const nav = createElement('nav', 'nav hidden', menu);
           const list = createElement('ul', 'menu__list', nav);
           const item1 = createElement('li', 'menu__item', list);
           item1.textContent = 'Тестирование';
