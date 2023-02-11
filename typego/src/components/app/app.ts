e1befa052b11a66df3194dc525f887e3eed961a4
dikhorsun <dmitry.horsun@gmail.com>
2023-02-11 23:34:51 +0300

    refactor: update app

--- typego/src/components/app/app.ts
+++ typego/src/components/app/app.ts
@@ -1,16 +1,19 @@
 import Model from '../model/model';
 import Controller from '../controller/controler';
 import DropStartPage from '../drop-food-game/drop-start-page';
+import DropGamePage from '../drop-food-game/drop-game-page';
 
 class App {
   model;
   controller;
   dropStartPage;
+  dropGamePage;
 
   constructor() {
     this.model = new Model();
     this.controller = new Controller();
     this.dropStartPage = new DropStartPage();
+    this.dropGamePage = new DropGamePage();
   }
 
   run(): void {
