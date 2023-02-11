64532cefb7764a7c437e66ae144a8034750c004a
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2023-02-11 17:44:42 +0300

    init: header footer render

--- typego/src/components/app/app.ts
+++ typego/src/components/app/app.ts
@@ -1,5 +1,6 @@
 import Model from '../model/model';
 import Controller from '../controller/controler';
+import Header from '../view/header';
 import Main from '../view/main';
 import WhacAMole from '../view/whacamole';
 
@@ -8,15 +9,18 @@ class App {
   controller;
   main;
   whac;
+  header;
 
   constructor() {
     this.model = new Model();
     this.controller = new Controller();
     this.whac = new WhacAMole();
+    this.header = new Header();
     this.main = new Main();
   }
 
   run(): void {
+    this.header.run();
     this.main.run();
     // this.whac.run();
   }
