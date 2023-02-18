59614549e90c78622fb6aee10fb57cbd8c7f26cb
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2023-02-18 12:44:00 +0300

    feat: generate user profile header

--- typego/src/components/view/profile.ts
+++ typego/src/components/view/profile.ts
@@ -9,7 +9,7 @@ import whacamoleGameLogo from '../../assets/png/whacamole.png';
 import dropfoodGameLogo from '../../assets/png/drop-food.png';
 import gunGameLogo from '../../assets/png/gun-game.png';
 
-import certTemplate from '../../assets/png/certificate-template.png';
+// import certTemplate from '../../assets/png/certificate-template.png';
 
 // ! ГЕНЕРАЦИЯ СЕРТИФИКАТА НА КАНВЕ
 console.log(whacamoleGameLogo, dropfoodGameLogo, gunGameLogo);
@@ -103,7 +103,7 @@ const userData: IUser = {
 };
 
 class Profile extends PageView {
-  private render(): void {
+  private render(currentUser: IUser): void {
     const app: HTMLElement | null = document.querySelector('.app');
 
     if (app !== null) {
@@ -117,11 +117,57 @@ class Profile extends PageView {
         main.classList.remove('dark');
       }
       const wrapper = createElement('div', 'profile-wrapper', main);
-      wrapper.innerHTML = `${wrapper} ${speedImg} ${accuracyImg}, ${progressImg}`;
-      wrapper.innerHTML = `
+
+      // cert generator
+      // const canvas = document.createElement('canvas');
+      // const ctx = canvas!.getContext('2d');
+      // ctx!.canvas.width = 1024;
+      // ctx!.canvas.height = 768;
+
+      // const image = new Image();
+      // image.crossOrigin = 'anonymous';
+      // image.src = certTemplate;
+
+      // const drawImage = () => {
+      //   ctx!.drawImage(image, 0, 0, 1024, 768);
+      //   ctx!.font = 'italic bold 48px Arial';
+      //   ctx!.fillStyle = 'gray';
+      //   ctx!.textAlign = 'center';
+      //   ctx!.fillText('TypeGo', 512, 120);
+      //   ctx!.font = 'bold 48px Arial';
+      //   ctx!.fillStyle = 'black';
+      //   ctx!.fillText('Nikolay Kovalev', 512, 320);
+      //   ctx!.font = 'italic 26px Arial';
+      //   ctx!.fillText('за скорость набора на русском 290 зн.мин с точностью 98.3%', 512, 400);
+
+      //   ctx!.fillText('18.02.2023', 450, 620);
+
+      //   document.getElementById('canvas')!.style.backgroundImage = `url(${canvas.toDataURL()})`;
+      // };
+
+      // image.onload = () => {
+      //   drawImage();
+      // };
+
+      // document.querySelector('.cert-download')!.addEventListener('click', (event) => {
+      //   const btn = event.target as HTMLAnchorElement;
+      //   btn.href = canvas.toDataURL('image/jpg');
+      //   btn.download = 'Certificate - name.jpg';
+      // });
+
+      // cert generator
+
+      /*
+      lang: 'ru',
+      lesson: 1,
+      level: 3,
+      accuracy: 83,
+      speed: 115,
+      */
+
+      /*
       <div class="user__block">
-        <div class="user__profile-image">
-        </div>
+        <div class="user__profile-image"></div>
         <div class="user__profile-info">
           <div class="user__name">NK</div>
           <div class="user__statistics">
@@ -151,107 +197,53 @@ class Profile extends PageView {
           </div>
         </div>
       </div>
-      <div class="user__info">
-        <div class="info__block">
-          <h2>Сертификат</h2>
-          <div class="info__block-cert">
-            <div class="cert-message hidden">До получения сертификата осталось совсем чуть-чуть - пройти тест!</div>
-            <div class="cert-block">
-              <canvas class="cert-preview" id="canvas"></canvas>
-              <a class="cert-download">Скачать сертификат</a>
-            </div>
-          </div>
-        </div>
-        <div class="info__block ">
-          <h2>Уроки</h2>
-          <div class="info__block-lessons">
-            <div class="lesson__lang">
-              <div class="lesson__lang-caption">Уроки на <i>русском</i> языке:</div>
-              <div class="lesson__lang-lesson">
-                <div class="lesson__lang-lesson_caption">Урок <b>1</b>:</div>
-                <div class="lesson__lang-levels">
-                  <div class="lesson__lang-levels__line">
-                    <span>Уровень <b>1</b></span> -
-                    <span>Точность: <b>83</b>%</span>
-                    <span>Скорость: <b>115</b> зн/мин.</span>
-                  </div>
-                  <div class="lesson__lang-levels__line">
-                    <span>Уровень <b>2</b></span> -
-                    <span>Точность: <b>95</b>%</span>
-                    <span>Скорость: <b>103</b> зн/мин.</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="info__block">
-          <h2>Игры</h2>
-          <div class="info__block-games">
-            <div class="game-line">
-              <div class="game_line-image"></div>
-              <div class="game_line-info">
-                <div class="game_line-info_gamename">Ударь крота</div>
-                <div class="game_line-info_results">
-                  <div class="game_line-info_results-level"><b>3</b> уровень.</div>
-                  <div class="game_line-info_results-score"><b>120</b> попаданий.</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      `;
-
-      const canvas = document.createElement('canvas');
-      const ctx = canvas!.getContext('2d');
-      ctx!.canvas.width = 1024;
-      ctx!.canvas.height = 768;
-
-      const image = new Image();
-      image.crossOrigin = 'anonymous';
-      image.src = certTemplate;
-
-      const drawImage = () => {
-        ctx!.drawImage(image, 0, 0, 1024, 768);
-        ctx!.font = 'italic bold 48px Arial';
-        ctx!.fillStyle = 'gray';
-        ctx!.textAlign = 'center';
-        ctx!.fillText('TypeGo', 512, 120);
-        ctx!.font = 'bold 48px Arial';
-        ctx!.fillStyle = 'black';
-        ctx!.fillText('Nikolay Kovalev', 512, 320);
-        ctx!.font = 'italic 26px Arial';
-        ctx!.fillText('за скорость набора на русском 290 зн.мин с точностью 98.3%', 512, 400);
-
-        ctx!.fillText('18.02.2023', 450, 620);
-
-        document.getElementById('canvas')!.style.backgroundImage = `url(${canvas.toDataURL()})`;
-      };
-
-      image.onload = () => {
-        drawImage();
-      };
-
-      document.querySelector('.cert-download')!.addEventListener('click', (event) => {
-        const btn = event.target as HTMLAnchorElement;
-        btn.href = canvas.toDataURL('image/jpg');
-        btn.download = 'Certificate - name.jpg';
-      });
-
-      /*
-      lang: 'ru',
-      lesson: 1,
-      level: 3,
-      accuracy: 83,
-      speed: 115,
       */
+      const userBlock = createElement('div', 'user__block', wrapper);
+      createElement('div', 'user__profile-image', userBlock);
+      const userProfileInfo = createElement('div', 'user__profile-info', userBlock);
+      const userName = createElement('div', 'user__name', userProfileInfo);
+      userName.textContent = currentUser.username;
+      const userStatistics = createElement('div', 'user__statistics', userProfileInfo);
+      // progress
+      const statisticsBlock1 = createElement('div', 'statistics__block', userStatistics);
+      if (currentUser.progress.length) {
+        const statisticsLogo = createElement('div', 'statistics__logo', statisticsBlock1);
+        statisticsLogo.style.background = `url(${progressImg}) center no-repeat`;
+        const statisticsProgress = createElement('div', 'statistics__progress', statisticsBlock1);
+        const userStatisticsTitle = createElement('span', 'user__statistics-title', statisticsProgress);
+        userStatisticsTitle.textContent = 'Прогресс';
+        const userStatisticsValue = createElement('span', 'user__statistics-value', statisticsProgress);
+        userStatisticsValue.innerHTML = `<b>${currentUser.progress.length}</b> уроков`;
+      }
+
+      // speed
+      const statisticsBlock2 = createElement('div', 'statistics__block', userStatistics);
+      if (currentUser.speed) {
+        const statisticsLogo = createElement('div', 'statistics__logo', statisticsBlock2);
+        statisticsLogo.style.background = `url(${speedImg}) center no-repeat`;
+        const statisticsAccuracy = createElement('div', 'statistics__accuracy', statisticsBlock2);
+        const userStatisticsTitle = createElement('span', 'user__statistics-title', statisticsAccuracy);
+        userStatisticsTitle.textContent = 'Скорость печати';
+        const userStatisticsValue = createElement('span', 'user__statistics-value', statisticsAccuracy);
+        userStatisticsValue.innerHTML = `<b>${currentUser.speed}</b> зн./мин`;
+      }
+
+      // accuracy
+      const statisticsBlock3 = createElement('div', 'statistics__block', userStatistics);
+      if (currentUser.accuracy) {
+        const statisticsLogo = createElement('div', 'statistics__logo', statisticsBlock3);
+        statisticsLogo.style.background = `url(${accuracyImg}) center no-repeat`;
+        const statisticsSpeed = createElement('div', 'statistics__speed', statisticsBlock3);
+        const userStatisticsTitle = createElement('span', 'user__statistics-title', statisticsSpeed);
+        userStatisticsTitle.textContent = 'Точность';
+        const userStatisticsValue = createElement('span', 'user__statistics-value', statisticsSpeed);
+        userStatisticsValue.innerHTML = `<b>${currentUser.accuracy}</b>%`;
+      }
     }
   }
 
   public run(): void {
-    this.render();
-    console.log(userData);
+    this.render(userData);
   }
 }
 
